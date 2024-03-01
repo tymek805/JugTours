baff228ee11f4775401faed1395fd761dad97c22
Tymoteusz Lango <58483383+tymek805@users.noreply.github.com>
2024-03-02 00:35:58 +0100

    Init commit

---
+++ src/test/java/com/tl/jugtours/JugToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tl.jugtours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugToursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
